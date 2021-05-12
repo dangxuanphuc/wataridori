e951005b720be1fc917e85345ec505e93c54264e
Dang Xuan Phuc <dangxuanphuc2211@gmail.com>
2021-05-12 23:28:00 +0700

    Static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = "Wataridori's Blog"
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
+
+  def active_class action
+    params[:action] == action ? "active" : nil
+  end
 end
