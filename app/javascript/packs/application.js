cf2b12b65bd1709963d11ac5f70d41f47a0f0bd9
Phuc Dang Xuan <d.phuc@id-entity.vn>
2021-05-21 13:45:01 +0700

    CRUD post

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import "jquery"
 global.$ = require("jquery")
 import "bootstrap"
 import "@fortawesome/fontawesome-free/css/all"
-import "moment"
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
