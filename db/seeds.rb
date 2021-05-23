602c2cb0768533321a7828832e78e2a95da7f2b6
Dang Xuan Phuc <dangxuanphuc2211@gmail.com>
2021-05-23 23:20:52 +0700

    Minor bug fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Skill.create!([
+  {name: "Vietnamese", percent: 100, skill_type: "language"},
+  {name: "Japanese", percent: 40, skill_type: "language"},
+  {name: "English", percent: 55, skill_type: "language"},
+  {name: "Ruby", percent: 60, skill_type: "technology"},
+  {name: "Javascript", percent: 50, skill_type: "technology"},
+  {name: "VueJS", percent: 45, skill_type: "technology"},
+  {name: "MySQL", percent: 55, skill_type: "technology"},
+  {name: "Docker", percent: 50, skill_type: "technology"},
+  {name: "CSS", percent: 75, skill_type: "technology"},
+])
