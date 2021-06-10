3a9e5476a18ed0be4b34b3393e6149ca865c0a57
Dang Xuan Phuc <dangxuanphuc2211@gmail.com>
2021-06-10 08:09:42 +0700

    Modify error pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "about-me", to: "static_pages#about"
   get "favorite-songs", to: "static_pages#song"
   get "emoticons", to: "static_pages#emoticon"
+  match "/404", to: "errors#error_404", via: :all
+  match "/422", to: "errors#error_422", via: :all
+  match "/500", to: "errors#error_500", via: :all
 
   resources :posts, param: :slug do
     member do
