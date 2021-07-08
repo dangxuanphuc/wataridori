182d068e7ec315608ebfd988a00981de8e65fffd
Dang Xuan Phuc <dangxuanphuc2211@gmail.com>
2021-07-08 22:54:56 +0700

    Play/Pause songs

--- app/javascript/packs/songs.js
+++ app/javascript/packs/songs.js
@@ -1,3 +1,23 @@
+var isPlaying = false;
+
+$(document).on("click", ".js--music-play", function(e) {
+  e.preventDefault();
+  $("#js--song").trigger("play");
+  $(".pause-song").removeClass("d-none")
+  $(".play-song").addClass("d-none")
+  $(this).removeClass("js--music-play").addClass("js--music-pause")
+  isPlaying = true
+})
+
+$(document).on("click", ".js--music-pause", function(e) {
+  e.preventDefault();
+  $("#js--song").trigger("pause");
+  $(".pause-song").addClass("d-none")
+  $(".play-song").removeClass("d-none")
+  $(this).removeClass("js--music-pause").addClass("js--music-play")
+  isPlaying = false
+})
+
 $(document).on("click", "#js--next-song", function(e) {
   e.preventDefault();
   const url = $(this).data("url");
@@ -7,7 +27,23 @@ $(document).on("click", "#js--next-song", function(e) {
     url: url,
     method: "GET",
     dataType: "SCRIPT",
-    data: { song_id: song_id, status: "next" }
+    data: { song_id: song_id, status: "next" },
+    success: function() {
+      if(isPlaying == true) {
+        setTimeout(function() {
+          $("#js--song").trigger("play");
+          $(".pause-song").removeClass("d-none")
+          $(".play-song").addClass("d-none")
+          $(this).removeClass("js--music-play").addClass("js--music-pause")
+          isPlaying = true
+        }, 1000);
+      } else {
+        $(".pause-song").addClass("d-none")
+        $(".play-song").removeClass("d-none")
+        $(this).removeClass("js--music-pause").addClass("js--music-play")
+        isPlaying = false
+      }
+    }
   })
 })
 
@@ -20,18 +56,26 @@ $(document).on("click", "#js--prev-song", function(e) {
     url: url,
     method: "GET",
     dataType: "SCRIPT",
-    data: { song_id: song_id, status: "prev" }
+    data: { song_id: song_id, status: "prev" },
+    success: function() {
+      if(isPlaying == true) {
+        setTimeout(function() {
+          $("#js--song").trigger("play");
+          $(".pause-song").removeClass("d-none")
+          $(".play-song").addClass("d-none")
+          $(this).removeClass("js--music-play").addClass("js--music-pause")
+          isPlaying = true
+        }, 1000);
+      } else {
+        $(".pause-song").addClass("d-none")
+        $(".play-song").removeClass("d-none")
+        $(this).removeClass("js--music-pause").addClass("js--music-play")
+        isPlaying = false
+      }
+    }
   })
 })
 
-// $(document).on("click", "#js--music", function(e) {
-//   e.preventDefault();
-//   const audio_url = $(this).data("audio");
-//   const audio = new Audio(audio_url);
-//   audio.play();
-//   $(".pause-song").removeClass("d-none")
-//   $(".play-song").addClass("d-none")
-//   if(audio.play()) {
-//     audio.pause();
-//   }
+// $("#js--song").on("ended", function() {
+//   $("#js--next-song").trigger("click");
 // })
