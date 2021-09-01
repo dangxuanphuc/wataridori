e6f274fd1323951a32452eca241a18b11ce12d57
Phuc Dang Xuan <d.phuc@id-entity.vn>
2021-09-01 14:07:21 +0700

    Fix share url

--- app/models/concerns/sns_share_url.rb
+++ app/models/concerns/sns_share_url.rb
@@ -3,8 +3,8 @@ module SnsShareUrl
 
   def sns_share_urls(post)
     {
-      twitter: format(Settings.share.twitter.share_url, url: post_path(post), text: "ワタリドリ") + "%0A",
-      facebook: format(Settings.share.facebook.dig(:share_url), url: post_path(post)),
+      twitter: format(Settings.share.twitter.share_url, url: post_url(post), text: "ワタリドリ") + "%0A",
+      facebook: format(Settings.share.facebook.dig(:share_url), url: post_url(post), text: "ワタリドリ"),
     }
   end
 end
