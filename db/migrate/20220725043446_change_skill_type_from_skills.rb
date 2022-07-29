51897c19b4648cb39fb928194714483e04c049fb
Phuc Dang Xuan <32502760+dangxuanphuc@users.noreply.github.com>
2022-07-29 21:59:24 +0700

    Fix datatype mismatch (#124)

--- db/migrate/20220725043446_change_skill_type_from_skills.rb
+++ db/migrate/20220725043446_change_skill_type_from_skills.rb
@@ -1,5 +1,5 @@
 class ChangeSkillTypeFromSkills < ActiveRecord::Migration[6.1]
   def change
-    change_column :skills, :skill_type, :integer, default: 1
+    change_column :skills, :skill_type, "integer USING CAST(skill_type AS integer)", default: 1
   end
 end
