5ad2db59963e54c8a0f58063eae85eccf909cccb
Phuc Dang Xuan <32502760+dangxuanphuc@users.noreply.github.com>
2023-08-09 14:42:32 +0700

    Update skill type (#152)
    Co-authored-by: phucdx <dang.xuan.phuc@vareal.vn>

--- db/migrate/20220725043446_change_skill_type_from_skills.rb
+++ db/migrate/20220725043446_change_skill_type_from_skills.rb
@@ -1,5 +1,5 @@
 class ChangeSkillTypeFromSkills < ActiveRecord::Migration[6.1]
   def change
-    change_column :skills, :skill_type, :integer
+    change_column :skills, :skill_type, 'integer USING CAST(skill_type AS integer)'
   end
 end
